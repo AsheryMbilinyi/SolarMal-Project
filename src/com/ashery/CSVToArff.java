dd4a500bfcb3faa58cc8b69b28dae0bb15f9dd5e
AsheryMbilinyi <a2nyi4@gmail.com>
2019-07-19 22:38:00 +0200

    converting csv to arff

---
+++ src/com/ashery/CSVToArff.java
@@ -0,0 +1,26 @@
+package com.ashery;
+
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+
+import java.io.File;
+
+public class CSVToArff {
+
+    public static void main(String[] args) throws Exception{
+        //load CSV
+        CSVLoader loader = new CSVLoader();
+        loader.setSource(new File("Input/malariaDataset.csv"));
+        Instances data = loader.getDataSet();
+
+
+        // save ARFF
+        ArffSaver saver = new ArffSaver();
+        saver.setInstances(data);
+        saver.setFile(new File("Input/malariaDataset.arff"));
+        saver.writeBatch();
+
+    }
+
+}
