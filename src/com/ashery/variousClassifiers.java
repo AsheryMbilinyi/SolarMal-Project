cd4297822642e1095d7640bb7c0ec6f6be4b9c3b
AsheryMbilinyi <a2nyi4@gmail.com>
2019-07-24 08:18:58 +0200

    classifiers

--- src/com/ashery/variousClassifiers.java
+++ src/com/ashery/variousClassifiers.java
@@ -2,12 +2,7 @@ package com.ashery;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.NaiveBayes;
-import weka.classifiers.meta.AdaBoostM1;
-import weka.classifiers.meta.Bagging;
 import weka.classifiers.meta.FilteredClassifier;
-import weka.classifiers.trees.J48;
-import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
@@ -42,75 +37,32 @@ public class variousClassifiers {
         return filteredClassifier;
     }
 
-
-    public Classifier getDecisionTree(){
-        J48 DecisionTree=new J48();
-        return DecisionTree;
-    }
-
-    public Classifier getNaiveBayes() {
-        NaiveBayes naiveBayes = new NaiveBayes();
-        naiveBayes.setUseSupervisedDiscretization(true);
-        return naiveBayes;
-    }
-    public Classifier getRandomForest() {
-        RandomForest randomForest = new RandomForest();
-        return randomForest;
-    }
-
-    public Classifier getAdaBoost(){
-        AdaBoostM1 m1=new AdaBoostM1();
-        m1.setClassifier(new RandomForest());
-        m1.setNumIterations(10);
-        return m1;
-    }
-
-
-    public Classifier getBagging(){
-        Bagging bags=new Bagging();
-        bags.setClassifier(new RandomForest());
-        bags.setNumIterations(5);
-        return bags;
-    }
-
-
-
     public static void main(String[] args) throws Exception{
         variousClassifiers variousClassifiers=new variousClassifiers();
-        variousClassifiers.initDataSets("Input/v2/dataForPrediction.arff");
-        PrintWriter out=new PrintWriter("Output/filteredBagging.txt");
+        variousClassifiers.initDataSets("Input/malariaDatasetNominal.arff");
+
         Filter missingValueReplacements=variousClassifiers.applyingReplaceMissingValueFilter();
-        Classifier bagging=variousClassifiers.getBagging();
-
-        Classifier fc=variousClassifiers.applyingFilterOnClassifier(missingValueReplacements,bagging);
-        fc.buildClassifier(variousClassifiers.train);
-        Evaluation evaluation=new Evaluation(variousClassifiers.train);
-        evaluation.evaluateModel(fc,variousClassifiers.test);
-
-        out.println(fc.toString());
-        out.println(evaluation.toSummaryString("Evaluation Results:Filtered Bagging",false));
-        out.println(evaluation.toMatrixString("===Overall Confusion Matrix\n"));
-        out.close();
+        Classifier classifier[] = {AdaBoost.getAdaBoost(),BaggingClassifier.getBagging(),
+                DecisionTree.getDecisionTree(), NaiveBayesClassifier.getNaiveBayes(),
+                RandomForestClassifier.getRandomForest(),SVM.SupportVector()};
 
 
+        for(int i=0;i<classifier.length;i++){
+
+		Classifier fc=variousClassifiers.applyingFilterOnClassifier(missingValueReplacements,classifier[i]);
+		fc.buildClassifier(variousClassifiers.train);
+		Evaluation evaluation=new Evaluation(variousClassifiers.train);
+		evaluation.evaluateModel(fc,variousClassifiers.test);
+		PrintWriter out=new PrintWriter("Output/"+i+".txt");
+		out.println(fc.toString());
+		out.println(evaluation.toSummaryString("Evaluation Results:",false));
+		out.println(evaluation.toMatrixString("===Overall Confusion Matrix\n"));
+		out.close();
+
+
+        }
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
